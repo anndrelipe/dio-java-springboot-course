161329284117000787fc444abced995a7e4301a3
André Peixoto <ppeixoto.andre@gmail.com>
2024-08-12 09:38:21 -0300

    Aula sobre tipos primitivos: byte, short, int, long, float, double, boolean, char

---
+++ Sintaxe_Java/Tipos_e_variaveis/TiposPrimitivos/src/TiposPrimitivos.java
@@ -0,0 +1,12 @@
+public class TiposPrimitivos {
+    public static void main (String [] args) {
+        char um_char = 'a';
+        int um_int = 128;
+        // Ainda existe Byte, Short, e Long (L);
+        double um_double = 3.14;
+        // Ainda existe Float (F)
+        boolean um_bolean = true;
+        String um_String = "Sou uma string"; 
+        // É uma classe, não tipo primitivo
+    }
+}
